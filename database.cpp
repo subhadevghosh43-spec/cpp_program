b8b23cc7e5f945f6a03a85e8a276fddc87b86382
Subhadev <subhadevghosh43@gmail.com>
2026-08-09 19:32:53 +0530

    Started to Write the Program

---
+++ database.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+     class Student_Data
+     {
+       public:
+              string name;
+              int Phone_no,Roll_no,Age,Class;
+              char Gender;
+
+       void database()
+       {
+         int no;
+           cout<<"Which Data do you want to retrieve about the student: "<<endl;
+           cout<<"Enter \n 1. For Complete Data about the student \n 2. For Name of the student \n 3. For Roll NO of the student \n";
+        {
+         cout<<"The Name of the Student is "<<name<<endl;
+         cout<<"The Class of the Student is "<<Class<<endl;
+         cout<<"The PhoneNo of the Student is "<<Phone_no<<endl;
+         cout<<"The Gender of the Student is "<<Gender;
+        }
+    }
+     };
+
+
+
+    
+     
+    int main()
+    {
+         int a,b,c;
+         
+
+
+
+    }
