d6f0c6510349acc30657f03dc9ffcdb4409620f6
Subhadev <subhadevghosh43@gmail.com>
2026-08-11 20:36:01 +0530

    New Additions

--- database.cpp
+++ database.cpp
@@ -14,14 +14,42 @@ using namespace std;
        {
          int no;
            cout<<"Which Data do you want to retrieve about the student: "<<endl;
-           cout<<"Enter \n 1. For Complete Data about the student \n 2. For Name of the student \n 3. For Roll NO of the student \n";
-        {
-         cout<<"The Name of the Student is "<<name<<endl;
-         cout<<"The Class of the Student is "<<Class<<endl;
-         cout<<"The PhoneNo of the Student is "<<Phone_no<<endl;
-         cout<<"The Gender of the Student is "<<Gender;
-        }
-    }
+           cout<<"Enter \n 1. For Complete Data about the student \n 2. For Name of the student \n 3. For Roll NO of the student \n 4. For Age of the student \n 5.For Phone No of the Student \n 6. For Gender of the student"<<endl;
+           cin>>no;
+            
+           switch (no)
+           {
+              case 1:
+                     cout<<"The Name of the Student is "<<name<<endl;
+                     cout<<"The Class of the Student is "<<Class<<endl;
+                     cout<<"The Roll NO of the student is "<<Roll_no<<endl;
+                     cout<<"The PhoneNo of the Student is "<<Phone_no<<endl;
+                     cout<<"The Gender of the Student is "<<Gender<<endl;
+                     cout<<"The Age of the Student is "<<Age<<endl;
+                     break;
+              case 2:
+                     cout<<"The Name of the Student is "<<name<<endl;
+                     break;
+              case 3:         
+                     cout<<"The Roll NO of the student is "<<Roll_no<<endl;
+                     break;
+              case 4:
+                     cout<<"The Age of the Student is "<<Age<<endl;
+                     break;
+              case 5:
+                     cout<<"The PhoneNo of the Student is "<<Phone_no<<endl;
+                     break;
+              case 6:
+                     cout<<"The Gender of the Student is "<<Gender<<endl;       
+                     break;
+              default:
+                      cout<<"You have entered an unlisted number or character, Please Try Again"<<endl;
+                   
+
+           }
+           
+       
+      }
      };
 
 
