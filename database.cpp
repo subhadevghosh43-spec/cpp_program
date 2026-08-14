ea498f7caa223f41b7f842b725528761cf499ca3
Subhadev <subhadevghosh43@gmail.com>
2026-08-14 19:45:50 +0530

    New Add

--- database.cpp
+++ database.cpp
@@ -11,8 +11,13 @@ using namespace std;
               char Gender;
 
        void database()
-       {
-         int no,Count;
+       {     
+               int A=0;
+             
+              do
+              {
+        
+           int no,Count;
            cout<<"Which Data do you want to retrieve about the student: "<<endl;
            cout<<"Enter \n 1. For Complete Data about the student \n 2. For Name of the student \n 3. For Roll NO of the student \n 4. For Age of the student \n 5.For Phone No of the Student \n 6. For Gender of the student"<<endl;
            cin>>no;
@@ -45,9 +50,13 @@ using namespace std;
               default:
                       cout<<"You have entered an unlisted number or character, Please Try Again"<<endl;
                    
-
+               
            }
-           
+              
+             
+             cout<<"Press 1 to access more informations or press any other no to exit "<<endl;
+             cin>>A;
+            } while(A == 1);
        
       }
      };
@@ -70,7 +79,7 @@ using namespace std;
        {
             cout<<"For the Student with Serial No. "<<i+1<<endl;
 
-               cout<<"Enter The Namen :"<<endl;
+               cout<<"Enter The Name :"<<endl;
                getline(cin,name);
                cout<<"Enter The Roll :"<<endl;
                cin>>roll; 
@@ -91,13 +100,19 @@ using namespace std;
                     Data[i].Phone_no=phone_no;
                     Data[i].Age=age;
        }
+         int A;
+         do{
+            int ser; 
+              cout<<"Enter the serial No of the Student you want to find the Details about"<<endl;
+              cin>>ser;
 
+  
+               Data[ser-1].database();
+                 
+                cout<<"Enter 1 to continue or enter any other no to exit "<<endl;
+                cin>>A;
 
-
-
-
-
-
-
+              } while(A=1);
+      return 0;
 
     }
