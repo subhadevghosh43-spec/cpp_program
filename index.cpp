314f2cd121ced81badcaf2db47983a20c1a03240
Subhadev <subhadevghosh43@gmail.com>
2026-09-24 23:18:22 +0530

    Final vs

--- index.cpp
+++ index.cpp
@@ -1,43 +1,49 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-  int main()
-  {
-     int num[4]={12,10,20,30};
-     int temp,sum[4]={0,0,0,0,},rem,count=0;
-
-      for(int i=0;i<4;i++)
+int main()
+{
+    int tot;
+    cout<<" \nEnter the Total No of index you want :";
+    cin>>tot;
+  
+     vector<int> num(tot);
+      
+      for(int a=0;a<tot;a++)
        {
 
-            for(int temp=num[i];temp>0;temp=temp/10)
-              {
-               
-               rem=temp%10;
-               sum[i]=sum[i]+rem;
-                 
-              }
-           
-           if(sum[i]==i)
-           {
-
-              cout<<"\n Smallest Index whose Sum of the digit is the Index No is "<<i<<endl;
-              break;
-           }   
-           else
-           {
-            count=count+1;
-            continue;
-           }
-
+         cout<<"Enter The digit for Index No "<<a<<" :";
+         cin>>num[a];
+         
        }
-        if(count==4)
+
+    bool found=false;
+
+
+    for(int i = 0; i < tot; i++)
+    {
+        int sum = 0;
+
+        for(int temp = num[i]; temp > 0; temp = temp / 10)
         {
-
-           cout<<" \nThere is no Index whose sum of the digit is equal to it's Index No "<<endl;
-
+            int rem = temp % 10;
+            sum = sum + rem;
         }
 
+        if(sum == i)
+        {
+            cout << "\nSmallest Index whose Sum of the digit is the Index No is " << i << endl;
+            found=true;
+            break;
+        }
+       
+    }
+
+    if(!found)
+    {
+        cout << "\nThere is no Index whose sum of the digit is equal to its Index No"<< endl;
+    }
 
     return 0;
-  }
-
+}
